630472d959ca4aa181f174e9eacef37715ec6cdd
eleazar caqui <eleazar969mejia@gmail.com>
2025-06-04 17:14:42 -0500

    Ejercicio número dos y tres

---
+++ ejecicicios/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // // Variables
+        // int numero1, numero2, numero3, numero4;
+        // double promedio;
+        // // Entrada
+        // Console.WriteLine("Ingresa el primer número entero: "); numero1 = int.Parse(Console.ReadLine());
+        // Console.WriteLine("Ingresa el segundo número entero: "); numero2 = int.Parse(Console.ReadLine());
+        // Console.WriteLine("Ingresa el tercer número entero: "); numero3 = int.Parse(Console.ReadLine());
+        // Console.WriteLine("Ingresa el cuarto número entero: "); numero4 = int.Parse(Console.ReadLine());
+        // // Calcular suma
+        // int suma = numero1 + numero2 + numero3 + numero4;
+        // // Calcular promedio
+        // promedio = suma / 4.0;
+        // // Salida
+        // Console.WriteLine("La suma de los números es: " + suma);
+        // Console.WriteLine("El promedio de los números es: " + promedio);
+        // Console.ReadKey();
+
+
+        double precio, cantidad, total;
+        // Entrada
+        Console.Write("Ingrese el precio por unidad: ");
+         precio = double.Parse(Console.ReadLine());
+        Console.Write("Ingrese la cantidad comprada: ");
+        cantidad = double.Parse(Console.ReadLine());
+        // Salida
+        total = precio * cantidad;
+        Console.WriteLine("El total a pagar es: S/" + total);
+        Console.ReadKey();
+    }
+}
